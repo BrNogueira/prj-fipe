40a2e1b69ee8b47eb402c6b70f20ecbaef5951e0
RODRIGO <rodrigo.nfutado@gmail.com>
2020-01-23 22:50:22 -0300

    ajustes layout

---
+++ oindex.php
@@ -0,0 +1,262 @@
+    <?php include('header.php'); ?>
+
+
+    <div class="container cards-home">
+        <div class="row">
+            <div class="row-fluid-wrapper col-md-4" id="card-promo">
+                <h1 class="title-brief">Depoimentos +</h1>
+                <span class="title-line"></span>
+                    <div class="card-item">
+                        <h3>Disponibilidade</h3>
+                        <p> Escolhi a mesma faculdade em todos os cursos realizados, devido à responsabilidade…</p>
+                    </div>  
+                    <div class="card-item">
+                        <h3>Descomplicado</h3>
+                        <p> Durante os 7 meses de curso, a gestora ofereceu todo apoio que o estudante precisa para
+                            concluir a graduação devidamente…</p>
+                    </div>  
+                    <div class="card-item">
+                        <h3>Satisfação</h3>
+                        <p>Gostaria de deixar registrada minha satisfação com a faculdade e o excelente curso de
+                        gestão que acabei de concluir…</p>
+                    </div>  
+            </div>
+            <div class="row-fluid-wrapper col-md-4" id="card-news">
+                <h1 class="title-news">Notícias +</h1>
+                <span class="title-line"></span>
+                    <div class="card-item">
+                        <h3>Black Friday: Cursos Livres com 20% de desconto</h3>
+                        <p>Saiba como funcionará a promoção</p>
+                    </div> 
+                    <div class="card-item">
+                        <h3>Vestibular 2020 – 2ª Chamada</h3>
+                        <p> Confira os aprovados em 2ª chamada no Vestibular 2020</p>
+                    </div> 
+                    <div class="card-item">
+                        <h3>Fipe lamentao falecimento do apresentador e empresário Augusto Liberato</h3>
+                        <p>A Instituição agradece por todacontribuição deste grande comunicador</p>
+                    </div> 
+            </div>
+            <div class="row-fluid-wrapper col-md-4" id="card-event">
+                <h1 class="title-event">Eventos/Cursos Livres +</h1>
+                <span class="title-line"></span>
+                    <div class="card-item">
+                        <h3>Gerenciamento de Crise</h3>
+                        <p>Aprenda a análise estratégica com os principais conceitos e práticas de comunicação em
+                            situação de crise</p>
+                    </div>
+                    <div class="card-item">
+                        <h3>Jornalismo de Viagem</h3>
+                        <p> Desenvolva técnicas de reportagem e texto compartilhando experiências da estrada</p>
+                    </div>
+                    <div class="card-item">
+                        <h3>SEO: Como impulsionar seusresultados no Marketing Digital – Fundamentos</h3>
+                        <p> Planeje o processo de otimização de sites</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="blog-cards-home">
+        <div class="row">
+            <div class="container">
+                <!-- Slider main container -->
+                <div class="swiper-container">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <div class="swiper-slide">
+                            <div class="card">
+                                <img src="assets/img/fotocurso1.png" alt="banner-curso">
+                                <div class="card-body">
+                                    <h5 class="card-title">MBA Economia, Setor Financeiro e Finanças Empresariais </h5>
+                                    <p class="card-text">27 de Abril de 2020</p>
+                                    <a href="#" class="btn btn-card">Quero me inscrever</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="card">
+                                <img src="assets/img/fotocurso2.png" alt="banner-curso">
+                                <div class="card-body">
+                                    <h5 class="card-title">MBA Gestão de Fundos de Investimento </h5>
+                                    <p class="card-text">14 de Abril de 2020</p>
+                                    <a href="#" class="btn btn-card">Quero me inscrever</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="card">
+                                <img src="assets/img/fotocurso3.png" alt="banner-curso">
+                                <div class="card-body">
+                                    <h5 class="card-title">MBA Gestão de Riscos Financeiros, Corporativos e Compliance </h5>
+                                    <p class="card-text">13 de Abril de 2020</p>
+                                    <a href="#" class="btn btn-card">Quero me inscrever</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="card">
+                                <img src="assets/img/fotocurso1.png" alt="banner-curso">
+                                <div class="card-body">
+                                    <h5 class="card-title">MBA Economia, Setor Financeiro e Finanças Empresariais </h5>
+                                    <p class="card-text">27 de Abril de 2020</p>
+                                    <a href="#" class="btn btn-card">Quero me inscrever</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="card">
+                                <img src="assets/img/fotocurso2.png" alt="banner-curso">
+                                <div class="card-body">
+                                    <h5 class="card-title">MBA Gestão de Fundos de Investimento </h5>
+                                    <p class="card-text">14 de Abril de 2020</p>
+                                    <a href="#" class="btn btn-card">Quero me inscrever</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="card">
+                                <img src="assets/img/fotocurso3.png" alt="banner-curso">
+                                <div class="card-body">
+                                    <h5 class="card-title">MBA Gestão de Riscos Financeiros, Corporativos e Compliance </h5>
+                                    <p class="card-text">13 de Abril de 2020</p>
+                                    <a href="#" class="btn btn-card">Quero me inscrever</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+                
+            <div class="cards-pagination">
+
+                <div class="swiper-button-prev pagination-btn"></div>
+                <div class="swiper-button-next pagination-btn"></div>
+                
+            </div>
+
+        </div>
+    </div>
+
+
+    <section class="container-news">
+        <div class="wrapper container">
+            <div class="line-up"></div>
+            <div class="form-send-email row">
+                <div class="col-lg-6 col-12">
+                    <span>Informe seu e-mail para receber informações da EESFipe:</span>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <form method="POST" action="#">
+                        <input type="hidden" name="_token" value="AqTncH9CtiNO0bBqeSST5TBRZNlnkIcCAtVPOtYZ">
+                        <input type="email" name="email" placeholder="Seu melhor e-mail">
+                        <button type="submit">Quero Receber!</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    
+
+<footer id="footer">
+    <section class="container">
+        <div class="wrapper">
+            <div class="row col-md-12">
+                <div class="col-md-2">
+                    <h5>ACESSO RAPIDO</h5>
+                    <p class="menu-footer"></p>
+                    <ul>
+                        <li><a href="#">In Company</a></li>
+                        <li><a href="#">Ouvidoria</a></li>
+                        <li><a href="#">Bolsa de Estudos</a></li>
+                        <li><a href="#">Eventos Gratuitos</a></li>
+                        <li><a href="#">Biblioteaca</a></li>
+                        <li><a href="#">Anpec</a></li>
+                        <li><a href="#">Politica de Privacidade</a></li>
+                        <li><a href="#" class="btn btn-card">Área do ex-aluno</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <h5>LINKS IMPORTANTES</h5>
+                    <p class="menu-footer"></p>
+                    <ul>
+                        <li><a href="#">> MEC</a></li>
+                        <li><a href="#">> FIPE</a></li>
+                        <li><a href="#">> Tabela de Veículos</a></li>
+                </div>
+                <div class="col-md-3">
+                    <img id="logo" class="row" src="assets/img/logo-fipe.png" alt="Logo">
+                    <p>Avenida Paulista, 1499 - 4º andar - Bela Vista <br> (Entrada pela Al. Casa Branca, 35) <br> São Paulo/SP - Brasil - CEP 01311-928</p>
+                    <div class="social-icons row">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    </div>
+                    <div class="box-direitos">
+                        <img class="direitos" src="assets/img/direitos.png" alt="direitos">
+                    </div>
+
+                </div>
+                <div class="row col-md-5 google-maps">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.1669481006966!2d-46.65822378502221!3d-23.562446784682304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c92cc95b15%3A0xc9286765f42af241!2sAlameda%20Casa%20Branca%2C%2035%20-%201499%20-%204%C2%BA%20andar%20(Entrada%20-%20Jardim%20Paulista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001408-001!5e0!3m2!1spt-BR!2sbr!4v1576376190325!5m2!1spt-BR!2sbr"
+                        width="536" height="290" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                </div>
+            </div>
+        </div>
+    
+</footer>
+</main>
+<script>
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView:1,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+        breakpoints: {
+    320: {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    },
+    768: {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    },
+    1024: {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    },
+    1440: {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    },
+    1900: {
+    slidesPerView: 3,
+    spaceBetween: 50
+    },
+    2560: {
+    slidesPerView: 3,
+    spaceBetween: 60
+    }
+}
+});
+</script>
+
+</body>
+<!-- SCRIPTS -->
+
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="https://use.fontawesome.com/0555129a6a.js"></script>
+<script src="assets/js/main.js"></script>
+
+</html>
